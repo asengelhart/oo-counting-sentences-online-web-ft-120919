518f678202627966fc49b5bf9ca3cdbde69f705a
Alex Engelhart <asengelhart@yahoo.com>
2019-12-18 17:59:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_string = self.split(" ")
+    count = 0 
+    split_string.each do |substring|
+      count += 1 if (substring.sentence? || substring.question? || substring.exclamation?)
+    end 
+    count 
   end
 end
